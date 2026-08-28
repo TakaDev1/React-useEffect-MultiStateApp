4284e0e27f824cbb99f07854108b3f0fac5f6a37
Izumi Takayuki <r8quattrospider@gmail.com>
2026-08-28 09:58:45 +0900

    feat: Created App Component

---
+++ src/components/DisplayState.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DisplayState = () => {
+  return <div>DisplayState</div>;
+};
+
+export default DisplayState;
---
+++ src/components/HandleCounter.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HandleCounter = () => {
+  return <div>HandleCounter</div>;
+};
+
+export default HandleCounter;
